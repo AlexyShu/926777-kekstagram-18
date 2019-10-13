766064163fbb6e42a7a70722794d933b65bc109d
AlexyShu <Za9i@bk.ru>
2019-10-13 15:50:08 +0300

    Задание: 14. Личный проект: надо подкачаться

--- js/xhr.js
+++ js/xhr.js
@@ -5,6 +5,19 @@
   var URL = 'https://js.dump.academy/kekstagram/data';
   var photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
   var blockPictures = document.querySelector('.pictures');
+  // Блок: Сообщение с ошибкой загрузки изображения
+  var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var errorPopup = errorTemplate.cloneNode(true);
+  var errorPopupTitle = errorPopup.querySelector('.error__title');
+  var errorPopupButtons = errorPopup.querySelector('.error__buttons');
+  var errorPopupTryAgainBtn = errorPopupButtons.children[0];
+
+  function onTryAgainBtnClick() {
+    window.location.reload();
+  }
+
+  errorPopupButtons.removeChild(errorPopupButtons.children[1]);
+  errorPopupTryAgainBtn.addEventListener('click', onTryAgainBtnClick);
 
 
   // функция случайных лайков и аватарок
@@ -24,7 +37,6 @@
   xhr.responseType = 'json';
 
   xhr.addEventListener('load', function () {
-    var error;
     switch (xhr.status) {
       case 200:
         var photos = xhr.response;
@@ -39,18 +51,18 @@
         }
         blockPictures.appendChild(fragment);
         break;
+      case 300:
+        errorPopupTitle.textContent = 'Ошибка: Запрос был перенаправлен сервером.';
+        break;
       case 400:
-        error = 'Неверный запрос';
+        errorPopupTitle.textContent = 'Ошибка в запросе.';
         break;
       case 404:
-        error = 'Ничего не найдено';
+        errorPopupTitle.textContent = 'Ошибка в запросе.';
         break;
       case 500:
-        error = 'Ошибка сервера';
+        errorPopupTitle.textContent = 'Ошибка: Проблемы на сервере, повторите позже..';
         break;
-
-      default:
-        error = 'Cтатус ответа: : ' + xhr.status + ' ' + xhr.statusText;
     }
   });
 
